00fbb3b4cb6d9b79988ef1f3b9182c4429d414da
MICHELLEWAIRIMU04 <michelle@michelle>
2024-09-25 13:02:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+
+const currentUser = "Grace Hopper"
+const welcomeMessage = 'Welcome to Flatbook'
+console.log(`${welcomeMessage}! ${currentUser}`) 
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '
+const upperCaseCurrentUser = currentUser.toUpperCase(); 
+console.log(`${excitedWelcomeMessage} ${upperCaseCurrentUser}!`)
+
+const shortGreeting = 'Welcome, '
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+console.log(`${shortGreeting} ${firstInitial}!`)
+

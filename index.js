ab4c551358aab90b24554e2d008ac01aa2e2ea60
MICHELLEWAIRIMU04 <michelle@michelle>
2024-10-01 20:13:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const currentUser = "Grace Hopper"
-const welcomeMessage = 'Welcome to Flatbook'
-console.log(`${welcomeMessage}! ${currentUser}`) 
+const welcomeMessage = "Welcome to Flatbook!"
+console.log(`${welcomeMessage} ${currentUser}`) 
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK!"
 const upperCaseCurrentUser = currentUser.toUpperCase(); 
 console.log(`${excitedWelcomeMessage} ${upperCaseCurrentUser}!`)
 
-const shortGreeting = 'Welcome, '
+const shortGreeting = "Welcome!"
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
 console.log(`${shortGreeting} ${firstInitial}!`)

9e63705449ca6b2d02d81014c9c2c12bd256a4ff
MICHELLEWAIRIMU04 <michelle@michelle>
2024-10-02 19:46:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log(`${currentUser}`)
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 console.log(`${welcomeMessage}`) 
 
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}! `
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK! ${currentUser.toUpperCase()}! `
 console.log(`${excitedWelcomeMessage}`)
 
 const shortGreeting = `Welcome, ${currentUser[0]}!`
